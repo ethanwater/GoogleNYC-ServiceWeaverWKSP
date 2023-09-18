22f04907a1cc37da07240f5474b3bdf79a33100e
ethanwater <angelshatepop@gmail.com>
2023-09-18 15:35:51 -0400

    https+curl

--- serviceweaver.go
+++ serviceweaver.go
@@ -3,9 +3,13 @@ package main
 import (
 		"context"
 		"fmt"
+		_ "embed"
+		"net/http"
+		"encoding/json"
 
 		"github.com/ServiceWeaver/weaver"
 )
+var indexHtml string
 
 func main(){
 	if err := weaver.Run(context.Background(), run); err != nil {
@@ -16,16 +20,34 @@ func main(){
 type app struct {
 	weaver.Implements[weaver.Main]
 	searcher weaver.Ref[Searcher]
-	lis weaver.Listener `weaver:"emoji"`
+	listener weaver.Listener `weaver:"emoji"`
 }
 
 func run(ctx context.Context, a *app) error {
-	a.Logger(ctx).Info("listener active", "addr", a.lis)
-	emojis, err := a.searcher.Get().Search(ctx, "city")
-	if err != nil {
-		return err
-	}
+	a.Logger(ctx).Info("listener active", "addr", a.listener)
 
-	fmt.Println(emojis)
-	return nil
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { 
+ 		if r.URL.Path != "/" { 
+ 			http.NotFound(w, r) 
+ 			return 
+ 		} 
+ 		fmt.Fprint(w, indexHtml) 
+ 	})
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query().Get("q")
+		emojis, err := a.searcher.Get().Search(ctx, query)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return 
+		}
+		bytes, err := json.Marshal(emojis) 
+ 		if err != nil { 
+ 			http.Error(w, err.Error(), http.StatusInternalServerError) 
+ 			return 
+ 		} 
+ 		fmt.Fprintln(w, string(bytes))
+	})
+
+	return http.Serve(a.listener, nil) 
 }
